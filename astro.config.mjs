65503f7a106e0b24cc617ac608c77b0c3f3243e1
HerreraCesar <herrera.cesar.arg@gmail.com>
2025-02-01 01:28:34 -0300

    fix: update gh action

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+// TODO: usar ssr
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
